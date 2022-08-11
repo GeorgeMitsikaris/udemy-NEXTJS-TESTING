9c09cfab5aee4bc594eb1c49843fe88534fc8417
George Mitsikaris <george.mitsikaris@gmail.com>
2022-08-11 15:02:23 +0300

    Added tests for background image and title in Home component

---
+++ base-concert-venue/__tests__/ui/home.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+
+import Home from "@/pages";
+
+test("renders background image and title", () => {
+  render(<Home />);
+  const heading = screen.getByRole("heading", {
+    name: "Welcome to Popular Concert Venue",
+  });
+  expect(heading).toBeInTheDocument();
+
+  const image = screen.getByRole("img", {
+		name: "Concert goer with hands in the shape of a heart",
+  });
+  expect(image).toBeInTheDocument();
+});
